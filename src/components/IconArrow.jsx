44ef8122d35eb0b4adc2d766547099aaf7ca5d4f
coresyt <corejsyt2011@gmail.com>
2024-11-24 18:10:35 -0600

    Change config of tailwindcss

---
+++ src/components/IconArrow.jsx
@@ -0,0 +1,12 @@
+export default function IconArrow() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-3'>
+      <path
+        fill='none'
+        stroke='#FFF'
+        strokeWidth='2'
+        d='M1 1l8.836 8.836L1 18.671'
+      />
+    </svg>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,18 @@ export default {
     extend: {
       colors: {
         primary: {
-          desaturatedRed: "hsl(0, 36%, 70%)",
-          softRed: "hsl(0, 93%, 68%)"
+          desaturatedRed: "rgb(179, 111, 111)", // hsl(0, 36%, 70%)
+          softRed: "rgb(239, 85, 85)"          // hsl(0, 93%, 68%)
         },
-        'neutral-darkGrayishRed': "hsl(0, 6%, 24%)"
+        'neutral-darkGrayishRed': "rgb(61, 46, 46)" // hsl(0, 6%, 24%)
       },
       fontFamily: {
         sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        bgPatternDesktop: '/bg-pattern-desktop.svg',
+        gradientLine1: 'linear-gradient(135deg, rgb(255, 255, 255), rgb(255, 250, 250))', // hsl(0, 0%, 100%), hsl(0, 100%, 98%)
+        gradientLine2: 'linear-gradient(135deg, rgb(255, 204, 204), rgb(245, 153, 153))'  // hsl(0, 80%, 86%), hsl(0, 74%, 74%)
       },
     },
   },
