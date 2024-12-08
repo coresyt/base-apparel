8de9fe6debc3d489b24f699b0841375b015512ab
coresyt <corejsyt2011@gmail.com>
2024-12-08 12:59:03 -0600

    Create actions and style in error state

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,45 @@ import HeroDesktop from './assets/hero-desktop.jpg'
 import IconError from './components/IconError'
 import IconArrow from './components/IconArrow'
 import './App.pcss'
+import { useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import clsx from 'clsx'
+import { useEffect } from 'react'
 
 const RE_EMAIL =
   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
 
 export default function App() {
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState(false)
+  const [inputCls, setInputCls] = useState([])
+
+  useEffect(() => {
+    setInputCls([
+      twMerge(clsx(error ? 'text-red-600 my-[12.5px] mr-[15px]' : 'hidden')),
+      twMerge(clsx(error ? 'text-red-600' : 'hidden'))
+    ])
+  }, [error])
+
+  /**
+   * @param {React.FormEvent<HTMLFormElement>} ev
+   */
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    if (!email.match(RE_EMAIL)) {
+      setError(true)
+      return
+    }
+
+    setEmail('')
+    setTimeout(() => {
+      setError(!!error)
+    }, 10000)
+  }
+
+  useEffect
+
   return (
     <main className='container'>
       <header className='container__header'>
@@ -28,19 +62,22 @@ export default function App() {
           store. Add your email below to stay up-to-date with announcements and
           our launch deals.
         </p>
-        <form className='container__info__form'>
+        <form className='container__info__form' onSubmit={handleSubmit}>
           <div className='container__info__form__contain'>
             <input
               placeholder='Email Addres'
               className='container__info__form__contain__input'
+              value={email}
+              onChange={(ev) => setEmail(ev.currentTarget.value)}
             />
-            <div hidden>
+            <div className={inputCls[0]}>
               <IconError />
             </div>
             <button className='container__info__form__contain__input__button'>
               <IconArrow />
             </button>
           </div>
+          <div className={inputCls[1]}>Please provide a valid email</div>
         </form>
       </section>
     </main>
